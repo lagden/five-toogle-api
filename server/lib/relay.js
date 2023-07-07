3437b92fab90b600ba6b89a2456b8d40c1033ad9
Thiago Lagden <lagden@gmail.com>
2023-07-07 02:22:11 -0300

    add board

--- server/lib/relay.js
+++ server/lib/relay.js
@@ -1,11 +1,25 @@
+import {RaspiIO} from 'raspi-io'
 import five from 'johnny-five'
 import ee from './ee.js'
 
-const relay = new five.Relay({
-	pin: 17,
+const board = new five.Board({
+	io: new RaspiIO(),
 })
 
+let relay
+
+board.on('ready', () => {
+	relay = new five.Relay({
+		pin: 17,
+	})
+})
+
+
 function toggle(v) {
+	if (relay === undefined) {
+		return
+	}
+
 	// Open the circuit.
 	if (v === false) {
 		relay.open()
